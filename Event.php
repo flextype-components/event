35e82b8bc6b19161bcd7106a4ec81eeca8266e49
Awilum <awilum@msn.com>
2018-04-26 15:01:46 +0300

    Fix for namespace

--- Event.php
+++ Event.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- * @package Flextype
+ * @package Flextype Components
  *
  * @author Sergey Romanenko <awilum@yandex.ru>
- * @link http://flextype.org
+ * @link http://components.flextype.org
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace Flextype;
+namespace Flextype\Component\Event;
 
 class Event
 {
